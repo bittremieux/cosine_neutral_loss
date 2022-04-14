0d84125751162d43da7d211cb63dbcb2722e25e1
Wout <wout@bittremieux.be>
2022-04-14 15:24:07 -0700

    Optimal modified cosine implementation
    it returns the optimal score, in contrast to the greedy implementation, although it is significantly slower (using SciPy). A custom implementation could be more efficient.

--- src/similarity.py
+++ src/similarity.py
@@ -3,6 +3,7 @@ from typing import List, Tuple
 
 import numba as nb
 import numpy as np
+import scipy.optimize
 import spectrum_utils.spectrum as sus
 
 import utils
@@ -40,6 +41,67 @@ def cosine(
 
 
 def modified_cosine(
+    spectrum1: sus.MsmsSpectrum,
+    spectrum2: sus.MsmsSpectrum,
+    fragment_mz_tolerance: float,
+) -> Tuple[float, List[Tuple[int, int]]]:
+    """
+    Compute the modified cosine similarity between the given spectra.
+
+    Parameters
+    ----------
+    spectrum1 : sus.MsmsSpectrum
+        The first spectrum.
+    spectrum2 : sus.MsmsSpectrum
+        The second spectrum.
+    fragment_mz_tolerance : float
+        The fragment m/z tolerance used to match peaks.
+
+    Returns
+    -------
+    Tuple[float, List[Tuple[int, int]]]
+        A tuple consisting of (i) the modified cosine similarity between both
+        spectra, and (ii) the indexes of matching peaks in both spectra.
+    """
+    intensity1 = np.copy(spectrum1.intensity) / np.linalg.norm(
+        spectrum1.intensity
+    )
+    intensity2 = np.copy(spectrum2.intensity) / np.linalg.norm(
+        spectrum2.intensity
+    )
+
+    # Only take peak shifts into account if the mass difference is relevant.
+    precursor_charge = max(spectrum1.precursor_charge, 1)
+    precursor_mass_diff = (
+        spectrum1.precursor_mz - spectrum2.precursor_mz
+    ) * precursor_charge
+    num_shifts = 1
+    if abs(precursor_mass_diff) >= fragment_mz_tolerance:
+        num_shifts += precursor_charge
+    mass_diff = np.zeros(num_shifts, np.float32)
+    for charge in range(1, num_shifts):
+        mass_diff[charge] = precursor_mass_diff / charge
+
+    # Construct pairwise cost matrix.
+    cost = np.zeros((len(spectrum1.mz), len(spectrum2.mz)), np.float32)
+    for i in range(len(spectrum1.mz)):
+        for j in range(len(spectrum2.mz)):
+            for shift in range(num_shifts):
+                if (
+                    abs(spectrum1.mz[i] - (spectrum2.mz[j] + mass_diff[shift]))
+                    <= fragment_mz_tolerance
+                ):
+                    cost[i, j] = max(intensity1[i] * intensity2[j], cost[i, j])
+
+    # Compute optimal assignment.
+    row_ind, col_ind = scipy.optimize.linear_sum_assignment(
+        cost, maximize=True
+    )
+    peak_matches = [(i, j) for i, j in zip(row_ind, col_ind) if cost[i, j] > 0]
+    return cost[row_ind, col_ind].sum(), peak_matches
+
+
+def modified_cosine_greedy(
     spectrum1: sus.MsmsSpectrum,
     spectrum2: sus.MsmsSpectrum,
     fragment_mz_tolerance: float,
